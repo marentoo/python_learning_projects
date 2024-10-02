a2a05d2430eb6f8dbbe9ac7130e79831d635d3b0
marento <mate.gusciora@gmail.com>
2024-10-02 15:45:49 +0200

    resst

---
+++ web_practice/TopFavMovie/fav_movies_data.py
@@ -0,0 +1,92 @@
+movies_data = [
+    {
+        "title": "Fight Club",
+        "year": 1999,
+        "description": "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+        "rating": 8.8,
+        "ranking": 10,
+        "review": "The raw energy and unpredictability of Tyler Durden make the movie unforgettable.",
+        "img_url": "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+    },
+    {
+        "title": "The Lord of the Rings: Trilogy",
+        "year": 2001,
+        "description": "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth.",
+        "rating": 8.8,
+        "ranking": 9,
+        "review": "Epic storytelling with timeless characters and stunning visuals.",
+        "img_url": "https://image.tmdb.org/t/p/w500/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg"
+    },
+    {
+        "title": "Star Wars: Saga",
+        "year": 1977,
+        "description": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+        "rating": 8.6,
+        "ranking": 8,
+        "review": "A genre-defining classic that set the standard for all space operas.",
+        "img_url": "https://image.tmdb.org/t/p/w500/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg"
+    },
+    {
+        "title": "The Guardian",
+        "year": 2006,
+        "description": "A high school swim champion with a troubled past enrolls in the U.S. Coast Guard's A School, where legendary rescue swimmer Ben Randall teaches him some hard lessons about loss, love, and self-sacrifice.",
+        "rating": 7.0,
+        "ranking": 7,
+        "review": "An inspiring and emotional story of mentorship and sacrifice.",
+        "img_url": "https://m.media-amazon.com/images/M/MV5BMjQxNTY0MzIzOV5BMl5BanBnXkFtZTgwODY1NDMyMDI@._V1_QL75_UX820_.jpg"
+    },
+    {
+        "title": "The Matrix: Trilogy",
+        "year": 1999,
+        "description": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        "rating": 8.7,
+        "ranking": 6,
+        "review": "Revolutionary special effects and a mind-bending storyline make this a must-watch.",
+        "img_url": "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg"
+    },
+    {
+        "title": "Mr. Nobody",
+        "year": 2009,
+        "description": "A boy stands on a station platform as a train is about to leave. Should he go with his mother or stay with his father? Infinite possibilities arise from this decision, as long as he doesn't choose, anything is possible.",
+        "rating": 7.8,
+        "ranking": 5,
+        "review": "A thought-provoking film about choices, time, and the nature of existence.",
+        "img_url": "https://m.media-amazon.com/images/M/MV5BMTg4ODkzMDQ3Nl5BMl5BanBnXkFtZTgwNTEwMTkxMDE@._V1_QL75_UX820_.jpg"
+    },
+    {
+        "title": "The Godfather",
+        "year": 1972,
+        "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        "rating": 9.2,
+        "ranking": 4,
+        "review": "A masterclass in storytelling, acting, and filmmaking.",
+        "img_url": "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+    },
+    {
+        "title": "Futurama",
+        "year": 2007,
+        "description": "Planet Express  adventure in years 3000's",
+        "rating": 7.6,
+        "ranking": 3,
+        "review": "What a universe.",
+        "img_url": "https://image.tmdb.org/t/p/original/7RRHbCUtAsVmKI6FEMzZB6Re88P.jpg"
+    },
+    {
+        "title": "Pulp Fiction",
+        "year": 1994,
+        "description": "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        "rating": 8.9,
+        "ranking": 2,
+        "review": "Quentin Tarantino's nonlinear masterpiece with unforgettable dialogue and characters.",
+        "img_url": "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    },
+    {
+        "title": "The Good, the Bad and the Ugly",
+        "year": 1966,
+        "description": "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
+        "rating": 8.8,
+        "ranking": 1,
+        "review": "A classic Western with incredible cinematography and iconic music.",
+        "img_url": "https://image.tmdb.org/t/p/w500/bX2xnavhMYjWDoZp1VM6VnU1xwe.jpg"
+    }
+]
--- web_practice/TopFavMovie/main.py
+++ web_practice/TopFavMovie/main.py
@@ -7,20 +7,95 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from sqlalchemy.exc import IntegrityError
+from fav_movies_data import movies_data
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
-# CREATE DB
+##CREATE DB
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
-# CREATE TABLE
+##CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+with app.app_context():
+    for movie_data in movies_data:
+        movie = Movie(
+            title=movie_data["title"],
+            year=movie_data["year"],
+            description=movie_data["description"],
+            rating=movie_data["rating"],
+            ranking=movie_data["ranking"],
+            review=movie_data["review"],
+            img_url=movie_data["img_url"]
+        )
+        db.session.add(movie)
+    
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            print(f"Movie {movie.title} already exist in the database.")
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    all_movies = result.scalars().all() #convert to list
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
+
+class RateForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10")
+    submit = SubmitField("Done")
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/delete")
+def delete():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+    
+
 
 
 if __name__ == '__main__':
